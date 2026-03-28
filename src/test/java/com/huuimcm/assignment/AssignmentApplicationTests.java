9ac581e9d2b89e8d48b40a1d72c817f29aa6a8d3
김도균 <kimdk0903@naver.com>
2026-03-28 13:12:24 +0900

    first commit

---
+++ src/test/java/com/huuimcm/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huuimcm.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
